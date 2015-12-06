8ab30b324d6b437bdd317045cc9c174b8dbb0f69
Christopher Wilson <cwilson@cdwilson.us>
2015-12-05 18:52:17 -0800

    Add distribution version comments

--- bustools/__init__.py
+++ bustools/__init__.py
@@ -1,1 +1,11 @@
-__version__ = '0.1.dev0'
+#==========================================================================
+# DISTRIBUTION VERSION
+#--------------------------------------------------------------------------
+# This is a PEP 0008 and PEP 0440 compliant version string, i.e.
+# "major.minor.micro".  It is read by setup.py to determine the
+# distribution version for public release on PyPI.
+#
+# For a discussion on single-sourcing the version across setup.py and
+# the project code, see
+# https://packaging.python.org/en/latest/single_source_version.html
+#==========================================================================
